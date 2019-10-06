4dc65ebdcc527abb31ef335dc3c02497f84aeacb
Soldier <soldiercrp@gmail.com>
2019-10-06 08:22:34 -0500

    fix: convert a host to a valid url

--- bin/is-website-vulnerable.js
+++ bin/is-website-vulnerable.js
@@ -5,7 +5,9 @@
 const { Audit, RenderConsole } = require('../index')
 
 const url = process.argv[2]
-if (!url) {
+const regExp = RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g)
+
+if (!regExp.test(url)) {
   console.error('')
   console.error('error: please provide a URL of a website to scan')
   console.error('')
