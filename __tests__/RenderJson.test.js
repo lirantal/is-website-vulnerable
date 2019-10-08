42320925ca73fb66375e120582cbabe1dbbf82e1
Rajkumar <rajkumaar2304@gmail.com>
2019-10-08 21:55:45 +0530

    feat(json): adds more tests
    Signed-off-by: Rajkumar <rajkumaar2304@gmail.com>

--- __tests__/RenderJson.test.js
+++ __tests__/RenderJson.test.js
@@ -10,4 +10,23 @@ describe('Vulnerability JSON renderer', () => {
     expect(vulnInfo.match(/"totalVulnerabilities": 14/)).toBeTruthy()
     expect(vulnInfo.match(/"executionTime": "23423 ms"/)).toBeTruthy()
   })
+
+  test('No vulnerability information should render an empty JSON', () => {
+    const renderer = new RenderJson({})
+    const vulnInfo = renderer.formatVulnerability()
+    expect(vulnInfo).toEqual('')
+  })
+
+  test('Printed JSON should also support no vulnerabilities existing', () => {
+    const results = require('./fixtures/no-vulns.json')
+    const renderer = new RenderJson(results)
+
+    const vulnInfo = renderer.format()
+    expect(
+      vulnInfo.match(
+        /No JavaScript libraries detected with publicly known security vulnerabilities/
+      )
+    ).toBeTruthy()
+    expect(vulnInfo.match(/"totalVulnerabilities": 0/)).toBeTruthy()
+  })
 })
