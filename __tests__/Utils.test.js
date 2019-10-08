637b56c4e0033f17b21862afd54ba40268a90805
Soldier <soldiercrp@gmail.com>
2019-10-08 11:54:50 -0500

    fix: typo

--- __tests__/Utils.test.js
+++ __tests__/Utils.test.js
@@ -6,12 +6,12 @@ describe('Utils', () => {
     expect(Utils.parseUrl(url)).toEqual(undefined)
   })
 
-  test('parseUrl function with url give without protocol', async () => {
+  test('parseUrl function with url given and without protocol', async () => {
     const url = 'google.com'
     expect(Utils.parseUrl(url)).toEqual('http://google.com')
   })
 
-  test('parseUrl function with url give with protocol', async () => {
+  test('parseUrl function with url given and with protocol', async () => {
     const url = 'https://google.com'
     expect(Utils.parseUrl(url)).toEqual('https://google.com')
   })
