9991422d126de8538f53a3edea5d49fc2377e400
Soldier <soldiercrp@gmail.com>
2019-10-08 11:48:09 -0500

    add: Utils test file

--- src/Utils.js
+++ src/Utils.js
@@ -2,6 +2,8 @@ const url = require('url')
 
 module.exports = {
   parseUrl: function(urlToScan) {
+    if (urlToScan === undefined) return urlToScan
+
     // eslint-disable-next-line node/no-deprecated-api
     if (url.parse(urlToScan).protocol === null) {
       urlToScan = 'http://' + urlToScan
