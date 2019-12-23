e3e74ff787c14e09ea81bd101129f763c5e8a0a9
Itai Steinherz <itaisteinherz@gmail.com>
2019-12-24 00:03:06 +0200

    Fix linting

--- bin/is-website-vulnerable.js
+++ bin/is-website-vulnerable.js
@@ -7,7 +7,7 @@ const debug = require('debug')('is-website-vulnerable')
 const argv = require('yargs').argv
 const { prompt } = require('enquirer')
 const isUrl = require('is-url-superb')
-const chalk = require("chalk")
+const chalk = require('chalk')
 const { Audit, RenderConsole, RenderJson, Utils } = require('../index')
 
 let url = process.argv[2]
@@ -18,13 +18,13 @@ const isJson = !!argv.json
 const showProgressBar = !isJson && !isWindows
 
 ;(async () => {
-  try {  
+  try {
     if (!url) {
       if (isJson) {
         throw new Error('Please provide a URL of a website to scan')
       } else {
         console.error('Woops! You forgot to provide a URL of a website to scan.')
-  
+
         try {
           const response = await prompt({
             type: 'input',
@@ -40,42 +40,42 @@ const showProgressBar = !isJson && !isWindows
     }
 
     if (!isUrl(url)) {
-      throw new Error("Given argument is an invalid URL")
+      throw new Error('Given argument is an invalid URL')
     }
-  
+
     url = Utils.parseUrl(url)
-  
+
     if (isWindows && !isJson) {
       console.log('Please wait, scanning the website (can take up to a minute)...')
     }
-  
+
     debug(`detecting isWindows: ${isWindows}`)
     debug(`detecting isJson: ${isJson}`)
     debug(`showing progress bar: ${isWindows}`)
-  
+
     const opts = {
       lighthouseOpts: {},
       chromeOpts: {}
     }
-  
+
     if (Utils.hasDevice(argv)) {
       opts.lighthouseOpts = Object.assign(opts.lighthouseOpts, {
         emulatedFormFactor: Utils.parseDevice(argv)
       })
     }
-  
+
     const chromePath = argv.chromePath
     if (chromePath) {
       opts.chromeOpts = Object.assign(opts.chromeOpts, { chromePath })
     }
-  
+
     const audit = new Audit()
-  
+
     const results = await audit.scanUrl(url, opts, showProgressBar)
     if (results.lhr.runtimeError) {
       throw new Error(results.lhr.runtimeError.message)
     }
-  
+
     let renderer
     if (argv.json) {
       renderer = new RenderJson(results, argv.jsLib)
@@ -83,10 +83,10 @@ const showProgressBar = !isJson && !isWindows
       renderer = new RenderConsole(results, argv.jsLib)
     }
     renderer.print()
-  
+
     process.exit(0)
   } catch (error) {
-    console.error(`\n${chalk.red.underline("Error")}: ${error.message}\n`)
+    console.error(`\n${chalk.red.underline('Error')}: ${error.message}\n`)
     console.error('Usage:\n  is-website-vulnerable https://www.google.com\n\n')
     process.exit(1)
   }
